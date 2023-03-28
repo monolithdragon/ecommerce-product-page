2299a51e0510ea9a73d0101bce565182d2e24f43
monolithdragon <joo.lajos76@gmail.com>
2023-03-28 19:51:04 +0200

    Added footer and navbar

---
+++ src/components/containers/footer/index.tsx
@@ -0,0 +1,22 @@
+function Footer() {
+  return (
+    <div className="space-x-1 text-center text-[11px]">
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+        className="text-[#3e52a3]"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by
+      <a href="https://www.frontendmentor.io/profile/monolithdragon" className="text-[#3e52a3]">
+        Joó Lajos
+      </a>
+      .
+    </div>
+  );
+}
+
+export default Footer;
