4fc90479880c229f41282e7a73a4a566027de0d2
monolithdragon <joo.lajos76@gmail.com>
2023-03-28 19:54:58 +0200

    Added left margin to links

--- src/components/containers/footer/index.tsx
+++ src/components/containers/footer/index.tsx
@@ -1,17 +1,20 @@
 function Footer() {
   return (
-    <div className="space-x-1 text-center text-[11px]">
+    <div className="text-center text-[11px]">
       Challenge by
       <a
         href="https://www.frontendmentor.io?ref=challenge"
         target="_blank"
         rel="noreferrer"
-        className="text-[#3e52a3]"
+        className="ml-1 text-[#3e52a3]"
       >
         Frontend Mentor
       </a>
       . Coded by
-      <a href="https://www.frontendmentor.io/profile/monolithdragon" className="text-[#3e52a3]">
+      <a
+        href="https://www.frontendmentor.io/profile/monolithdragon"
+        className="ml-1 text-[#3e52a3]"
+      >
         Joó Lajos
       </a>
       .
