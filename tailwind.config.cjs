a7acdf2488add97c71f33fbb3d26224af1b83a03
monolithdragon <joo.lajos76@gmail.com>
2023-03-29 12:44:46 +0200

    Added colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,37 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          primary: 'var(--color-primary)',
+          neutral: {
+            100: 'var(--color-neutral-light)',
+            200: 'var(--color-neutral)',
+            300: 'var(--color-neutral-dark)',
+            400: 'var(--color-neutral-very-dark)'
+          }
+        }
+      },
+      backgroundColor: {
+        skin: {
+          primary: {
+            100: 'var(--color-primary-alt)',
+            200: 'var(--color-primary)'
+          }
+        }
+      },
+      borderColor: {
+        skin: {
+          primary: 'var(--color-primary)'
+        }
+      },
+      ringColor: {
+        skin: {
+          primary: 'var(--color-primary)'
+        }
+      }
+    },
   },
   plugins: [],
 };
