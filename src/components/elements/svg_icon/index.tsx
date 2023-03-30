c57ef889816618efe988519b6ba7a4f1c83fae13
monolithdragon <joo.lajos76@gmail.com>
2023-03-30 17:39:58 +0200

    Deleted attributes

--- src/components/elements/svg_icon/index.tsx
+++ src/components/elements/svg_icon/index.tsx
@@ -1,7 +1,7 @@
 import { SvgIconProps } from './types';
 
 function SvgIcon({ Icon, ...rest }: SvgIconProps) {
-  return <Icon viewBox="0 0 24 24" width={24} height={24} {...rest} />;
+  return <Icon {...rest} />;
 }
 
 export default SvgIcon;
