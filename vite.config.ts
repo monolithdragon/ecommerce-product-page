0053ed69fb3fcb87daccb3f92bc81cef1f21d7e7
monolithdragon <joo.lajos76@gmail.com>
2023-03-30 08:49:05 +0200

    Added svgx plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types='vitest' />
+/// <reference types='@svgx/vite-plugin-react' />
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import EsLint from 'vite-plugin-linter';
 import tsConfigPaths from 'vite-tsconfig-paths';
 const { EsLinter, linterPlugin } = EsLint;
+import svgx from "@svgx/vite-plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
+export default defineConfig(async (configEnv) => ({
   base: '/ecommerce-product-page',
   plugins: [
     react(),
@@ -15,6 +18,9 @@ export default defineConfig((configEnv) => ({
     linterPlugin({
       include: ['./src}/**/*.{ts,tsx}'],
       linters: [new EsLinter({ configEnv })],
+    }),
+    svgx({
+      defaultImport: 'component'
     })
   ],
   test: {
