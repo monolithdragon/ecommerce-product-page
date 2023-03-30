49bf513774d1c813167d4e8f7bda52c6c58e0cb2
monolithdragon <joo.lajos76@gmail.com>
2023-03-30 17:41:11 +0200

    Added menu items

---
+++ src/shared/data/menuItems.ts
@@ -0,0 +1,24 @@
+import { IMenuItem } from "shared/interfaces/IMenuItem";
+
+export const menuItems: IMenuItem[] = [
+  {
+    title: 'Collections',
+    path: '#collections'
+  },
+  {
+    title: 'Men',
+    path: '#men'
+  },
+  {
+    title: 'Women',
+    path: '#women'
+  },
+  {
+    title: 'About',
+    path: '#about'
+  },
+  {
+    title: 'Contact',
+    path: '#contact'
+  },
+];
